473c23457fcae9bf2548a29754f1709aa5082225
Hamo <moahmedhassan21700@gmail.com>
2025-03-19 17:14:18 +0200

    Fix: Resolve Constants  import issue

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -7,7 +7,7 @@ import { yellowImg } from "../Utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models, sizes } from "../constants";
+import { models, sizes } from "../Constants";
 import { animateWithGsapTimeline } from "../Utils/animations";
 
 const Model = () => {
